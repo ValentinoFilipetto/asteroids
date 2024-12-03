e4e64f718edbf65baa1ee516c8d9fee796b92b3f
ValentinoFilipetto <valefilipetto@gmail.com>
2024-12-03 19:01:00 +0100

    feat: add player with some movement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -8,6 +9,9 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
     while True: 
         for event in pygame.event.get():
@@ -15,7 +19,13 @@ def main():
                 return
     
         screen.fill("black")
+        
+        player.draw(screen)
+        player.update(dt)
+
         pygame.display.flip()
+        time_since_last_thick = clock.tick(60)
+        dt = time_since_last_thick / 1000 # we divide by 1000 to convert to seconds
     
 
 if __name__ == "__main__":
