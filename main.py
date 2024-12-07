2c1c3ef050965d8951450a80b011235436518530
ValentinoFilipetto <valefilipetto@gmail.com>
2024-12-07 13:40:44 +0100

    feat: expand movements and create groups

--- main.py
+++ main.py
@@ -8,8 +8,11 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
@@ -17,14 +20,17 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        for item in updatable:
+            item.update(dt)
     
         screen.fill("black")
-        
-        player.draw(screen)
-        player.update(dt)
+
+        for item in drawable:
+            item.draw(screen)
 
         pygame.display.flip()
-        time_since_last_thick = clock.tick(60)
+        time_since_last_thick = clock.tick(60) # 60 is an upper bound
         dt = time_since_last_thick / 1000 # we divide by 1000 to convert to seconds
     
 
